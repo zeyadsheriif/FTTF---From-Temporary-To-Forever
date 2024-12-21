2c93e623ca086b509e20e8b7f0fdaf50e581f757
Zeyad Sherif <125796653+zeyadsheriif@users.noreply.github.com>
2024-12-21 17:11:03 +0200

    Delete cart directory

--- cart/context_processors.py
+++
@@ -1,4 +0,0 @@
-from .cart import Cart
-
-def cart_context(request): # create context processor so that the cart is working in all pages
-    return {'cart': Cart(request)} # return default data from the cart
